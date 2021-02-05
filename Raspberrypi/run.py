a8b7ad3fa278ca9ae2b6e559bb4186adefff54fd
inafi <irfan.k.nafi@gmail.com>
2021-02-05 15:44:24 -0500

    save

--- Raspberrypi/run.py
+++ Raspberrypi/run.py
@@ -24,7 +24,7 @@ camera.capture("send.jpg")
 print("Taken")
 
 os.system("sshpass -p 'Googlefair2019' scp send.jpg blindcv@13.72.104.19:~/")
-command = 'sshpass -p "Googlefair2019" ssh -t -oStrictHostKeyChecking=no "blindcv@13.72.104.19" "python darknet/exe.py && exit"'
+command = 'sshpass -p "Googlefair2019" ssh -t -oStrictHostKeyChecking=no "blindcv2@52.170.94.55" "python darknet/exe.py && exit"'
 
 p = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 text = p.stdout.read()
